53dec359914246049e430d06e70aeffa41bf0808
ManrajSingh02 <singhmanraj6702@gmail.com>
2026-05-19 22:34:49 +0530

    add dns

--- server/config/db.js
+++ server/config/db.js
@@ -1,22 +1,23 @@
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const client = new MongoClient(process.env.MONGO_URI);
+const client = new MongoClient(
+  process.env.MONGO_URI
+);
 
 let db;
 
-export const connectDB = async () => {
-  try {
+export const connectDB =
+  async () => {
+
     await client.connect();
 
-    db = client.db(process.env.DB_NAME);
+    db = client.db(
+      "courseEnrollmentDB"
+    );
 
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.log(error);
-  }
-};
+    console.log(
+      "MongoDB Connected"
+    );
+  };
 
 export const getDB = () => db;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from "express";
+
 import cors from "cors";
+
 import dotenv from "dotenv";
 
 import { connectDB } from "./config/db.js";
@@ -7,26 +9,68 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import courseRoutes from "./routes/courseRoutes.js";
 import enrollmentRoutes from "./routes/enrollmentRoutes.js";
-import adminRoutes from "./routes/adminRoutes.js";
 import dns from "node:dns";
 dotenv.config();
-dns.setServers(["0.0.0.0", "8.8.8.8"]);
+
 const app = express();
 
+dns.setServers(["0.0.0.0", "8.8.8.8"]);
+// ==========================
+// MIDDLEWARE
+// ==========================
 app.use(cors());
+
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/courses", courseRoutes);
-app.use("/api/enrollments", enrollmentRoutes);
-app.use("/api/admin", adminRoutes);
 
-const startServer = () => {
-  connectDB();
 
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on ${process.env.PORT}`);
+
+// ==========================
+// ROUTES
+// ==========================
+app.get("/api", (req, res) => {
+  res.json({
+    message: "API Running",
   });
-};
+});
 
-startServer();
+app.use(
+  "/api/auth",
+  authRoutes
+);
+
+app.use(
+  "/api/courses",
+  courseRoutes
+);
+
+app.use(
+  "/api/enrollments",
+  enrollmentRoutes
+);
+
+
+
+
+// ==========================
+// START SERVER
+// ==========================
+const PORT =
+  process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+
+    app.listen(PORT, () => {
+
+      console.log(
+        `Server running on port ${PORT}`
+      );
+    });
+
+  })
+  .catch((error) => {
+
+    console.log(error);
+
+  });
